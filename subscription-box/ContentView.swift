2fc1c3243335c09837c31be9ef982ccaf2321211
alaJream <jeremy.gonzales@students.dominican.edu>
2023-07-12 00:39:52 -0700

    Initial Commit

---
+++ subscription-box/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  subscription-box
+//
+//  Created by Jeremy Gonzales on 7/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ subscription-box/subscription_boxApp.swift
@@ -0,0 +1,17 @@
+//
+//  subscription_boxApp.swift
+//  subscription-box
+//
+//  Created by Jeremy Gonzales on 7/12/23.
+//
+
+import SwiftUI
+
+@main
+struct subscription_boxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
